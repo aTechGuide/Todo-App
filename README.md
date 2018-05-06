6923eb0fefb1f2e8674f38af89b48966516b560c
Kamran Ali <kamranalinitb@gmail.com>
2018-05-06 12:30:16 +0530

    Add POST Todo Route + Refactoring

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Todo API
+
+This APP is build while completing [this](https://www.udemy.com/the-complete-nodejs-developer-course-2) course on NodeJS
+
+## API
+POST http://localhost:3000/todos
+```
+{
+	"text": "My First TODO Item"
+}
+```
+
+# References
+[Mongoose Validation](http://mongoosejs.com/docs/validation.html)
---
+++ server/server.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var newTodo = new Todo({
+    text: req.body.text
+  });
+
+  newTodo.save().then((doc) => {
+    console.log(JSON.stringify(doc, undefined, 2));
+    res.send(doc);
+  }, (err) => {
+    res.status(400).send(err);
+    console.log('Unable to save Todo', err);
+  });
+
+})
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+})
+
+// var newUser = new User({
+//   email: 'abc@example.com'
+// });
+
+// newUser.save().then((doc) => {
+//   console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//   console.log('Unable to save newUser');
+// });
