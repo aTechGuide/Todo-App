af17b19579f75d4f9235c8d16fd259ae82c1e552
Kamran Ali <kamranalinitb@gmail.com>
2018-05-06 13:13:43 +0530

    Test POST Todos routs

---
+++ server/tests/server.test.js
@@ -0,0 +1,58 @@
+const expect = require('expect');
+const request = require('supertest');
+
+const {app} = require('./../server');
+const {Todo} = require('./../models/todo');
+
+beforeEach((done) => {
+  Todo.remove({}).then(() => {
+    done();
+  })
+});
+
+describe('POST /todos', () => {
+
+  it('Should create a new TODO', (done) => {
+    var text = 'Test todo text';
+
+    request(app)
+    .post('/todos')
+    .send({text: text})
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.text).toBe(text);
+    })
+    .end((err, res) => {
+      if (err) {
+        return done(err);
+      }
+
+      Todo.find().then((todos) => {
+        expect(todos.length).toBe(1);
+        expect(todos[0].text).toBe(text);
+        done();
+      }).catch((err) => {
+        done(e);
+      })
+    })
+  });
+
+  it('Should not create a new TODO with invalid body data', (done) => {
+
+    request(app)
+    .post('/todos')
+    .expect(400)
+    .end((err, res) => {
+      if (err) {
+        return done(err);
+      }
+
+      Todo.find().then((todos) => {
+        expect(todos.length).toBe(0);
+        done();
+      }).catch((err) => {
+        done(e);
+      })
+    })
+  });
+})
