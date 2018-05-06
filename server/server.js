d7193d89e4f9e85aa44d25e6dfae66446abc2548
Kamran Ali <kamranalinitb@gmail.com>
2018-05-06 13:29:11 +0530

    Add GET Todos Route

--- server/server.js
+++ server/server.js
@@ -24,6 +24,15 @@ app.post('/todos', (req, res) => {
 
 })
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (err) => {
+    res.status(400).send(err);
+    console.log('Unable to save Todo', err);
+  })
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 })
